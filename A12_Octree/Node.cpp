a23e955674a54a9d580ed9ea414797052153d9c4
WarpedTime <silverstorm79@gmail.com>
2017-05-07 15:12:30 -0400

    filled out update model list and updated node children to be list not array

--- A12_Octree/Node.cpp
+++ A12_Octree/Node.cpp
@@ -33,21 +33,56 @@ vector3 Node::getCenter()
 	return nodeCenter;
 }
 
-void Node::setChildren(Node * nodes)
+void Node::setChildren(Node* nodes)
 {
-	if (children != nullptr) {
-		delete[] children;
-		children = nodes;
-	}
-	else children = nodes;
+
+}
+
+void Node::addChild(Node* node) {
+	children.push_back(node);
 }
 
 void Node::updateModelList()
 {
+	vector<String*> possibleModels = parent->modelList;
+	vector<String*> containedModels = vector<String*>();
+
+	for (int i = 0; i < possibleModels.size(); i++) {
+		//check if each model is contained in node 
+		BoundingObjectClass* model = m_pBomMngr->GetBoundingObject(*possibleModels[i]);
+		bool isColliding = true;
+
+		//check model aabb collisions with octant
+		vector3 max = model->GetAABBMax;
+		vector3 min = model->GetAABBMin;
+
+		//Check for X
+		if (max.x < nodeMin.x)
+			isColliding = false;
+		else if (min.x > nodeMax.x)
+			isColliding = false;
+
+		//Check for Y
+		else if (max.y < nodeMin.y)
+			isColliding = false;
+		else if (min.y > nodeMax.y)
+			isColliding = false;
+
+		//Check for Z
+		else if (max.z < nodeMin.z)
+			isColliding = false;
+		else if (min.z > nodeMax.z)
+			isColliding = false;
+
+		//if is colliding add to node model list
+		if (isColliding) containedModels.push_back(possibleModels[i]);
+	}
+
+	modelList = containedModels;
 }
 
 bool Node::hasChildren()
 {
-	if (children == nullptr) return false;
-	else return true;
+	if (children.size > 0) return true;
+	else return false;
 }
